fdcf9c31ab1b8e334f364affac4492e795e7972d
KarinaLiashko <112810602+KarinaLiashko@users.noreply.github.com>
2024-08-18 18:50:30 +0200

    second partially

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import userData from "../userData.json";
 import Profile from "./Profile";
+import FriendList from "./FriendListItem";
+import friends from "../friends.json";
 
 export default function App() {
   return (
@@ -12,6 +14,7 @@ export default function App() {
         location={userData.location}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
     </>
   );
 }
---
+++ src/components/FriendListItem.jsx
@@ -0,0 +1,11 @@
+export default function FriendList({
+    {avatar, name, isOnline }
+}) {
+    return (
+        <div>
+  <img src="" alt="Avatar" width="48" />
+  <p>Friend name</p>
+  <p>Friend status</p>
+</div>
+    )
+}
